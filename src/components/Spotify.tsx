13bbb448ae0bb4c7aca916a9c2e55db7beaad1d4
Emigdio Torres <emigdio821@gmail.com>
2022-08-07 16:52:55 -0500

    refactor: Spotify status

--- src/components/Spotify.tsx
+++ src/components/Spotify.tsx
@@ -1,14 +1,33 @@
 import { BiMusic } from "react-icons/bi";
 import { FaSpotify } from "react-icons/fa";
 import useNowPlaying from "hooks/useNowPlaying";
-import { Flex, Text, Image, VStack } from "@chakra-ui/react";
+import { Flex, Text, Image, Stack, VStack, Tooltip } from "@chakra-ui/react";
+import { AnimatePresence } from "framer-motion";
+import styles from "assets/css/common.module.css";
 import MotionDiv from "./MotionDiv";
 // import useRecentlyPlayed from "hooks/useRecentlyPlayed";
+
+function SongTooltip({ children }: { children: React.ReactNode }) {
+  return (
+    <Tooltip
+      px={3}
+      hasArrow
+      rounded="md"
+      fontSize="xs"
+      arrowSize={6}
+      placement="bottom"
+      label="Refresh song data"
+    >
+      {children}
+    </Tooltip>
+  );
+}
 
 export default function Spotify() {
   const nowPlaying = useNowPlaying();
   // const recentlyPlayed = useRecentlyPlayed();
-  const { song, error } = nowPlaying;
+  const { playing, refreshSongData } = nowPlaying;
+  const { song, error } = playing;
 
   // if (!currSong) currSong = recentlyPlayed.song;
   // if (!error) error = recentlyPlayed.error;
@@ -16,41 +35,75 @@ export default function Spotify() {
   return (
     <>
       <Flex borderRadius="full" bg="dark" alignItems="center" direction="row">
-        <Image
-          mr={2}
-          alt="Album"
-          boxSize={20}
-          boxShadow="2xl"
-          objectFit="cover"
-          borderRadius="md"
-          src={song?.album.images[0].url}
-          fallback={
-            <VStack
-              w={20}
-              mr={2}
-              h={20}
-              bg="#333"
-              justify="center"
-              borderRadius="md"
-            >
-              <BiMusic color="white" size={18} />
-            </VStack>
-          }
-        />
+        <AnimatePresence exitBeforeEnter key={song?.album.images[0].url}>
+          <MotionDiv y={10}>
+            <SongTooltip>
+              <Image
+                mr={2}
+                alt="Album"
+                boxSize={20}
+                boxShadow="2xl"
+                cursor="pointer"
+                objectFit="cover"
+                _hover={{
+                  filter: "brightness(0.6)",
+                }}
+                _active={{
+                  filter: "brightness(1)",
+                }}
+                borderRadius="md"
+                onClick={() => refreshSongData()}
+                transition="all 0.2s ease-in-out"
+                src={song?.album.images[0].url || ""}
+                fallback={
+                  <SongTooltip>
+                    <VStack
+                      w={20}
+                      mr={2}
+                      h={20}
+                      bg="#333"
+                      cursor="pointer"
+                      justify="center"
+                      borderRadius="md"
+                      _hover={{
+                        filter: "brightness(0.6)",
+                      }}
+                      _active={{
+                        filter: "brightness(1)",
+                      }}
+                      transition="all 0.2s ease-in-out"
+                      onClick={() => refreshSongData()}
+                    >
+                      <BiMusic color="white" size={18} />
+                    </VStack>
+                  </SongTooltip>
+                }
+              />
+            </SongTooltip>
+          </MotionDiv>
+        </AnimatePresence>
         <Flex
           px={2}
           borderRadius="md"
           direction="column"
           alignItems="flex-start"
         >
-          <FaSpotify size={20} style={{ height: "24px" }} />
+          <Stack direction="row" align="center">
+            <FaSpotify
+              size={20}
+              style={{ height: "24px" }}
+              className={song ? styles["spotify-icon"] : undefined}
+            />
+          </Stack>
           {song ? (
-            <MotionDiv y={5}>
-              <Text fontWeight={600} noOfLines={2}>
-                {song.name}
-              </Text>
-              <Text>{song.artists[0]?.name}</Text>
-            </MotionDiv>
+            <AnimatePresence exitBeforeEnter key={song.name}>
+              <MotionDiv y={5}>
+                <Text fontWeight={600} noOfLines={2}>
+                  {song.name}
+                </Text>
+                <Text>{song.artists[0]?.name}</Text>
+              </MotionDiv>
+            </AnimatePresence>
           ) : (
             <>
               <Text fontWeight={600} noOfLines={1}>
--- src/hooks/useNowPlaying.ts
+++ src/hooks/useNowPlaying.ts
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 import { getNowPlaying } from "lib/spotify";
 
 interface NowPlayingState {
@@ -24,25 +24,30 @@ const initialState: NowPlayingState = {
 export default function useNowPlaying() {
   const [playing, setPlaying] = useState<NowPlayingState>(initialState);
 
-  useEffect(() => {
-    const nowPlaying = async () => {
-      const response = await getNowPlaying();
-      if (response.status === 204 || response.status > 400) {
-        setPlaying(initialState);
-      } else {
-        const res = await response.json();
+  const getNowPlayingData = useCallback(async () => {
+    const response = await getNowPlaying();
+    if (response.status === 204 || response.status > 400) {
+      setPlaying(initialState);
+    } else {
+      const res = await response.json();
 
-        if (res.error) {
-          setPlaying({ isPlaying: false, error: res.error.message });
-        } else if (res.item !== null) {
-          setPlaying({ isPlaying: true, song: res.item });
-        } else {
-          setPlaying(initialState);
-        }
+      if (res.error) {
+        setPlaying({ isPlaying: false, error: res.error.message });
+      } else if (res.item !== null) {
+        setPlaying({ isPlaying: true, song: res.item });
+      } else {
+        setPlaying(initialState);
       }
-    };
-    nowPlaying();
+    }
   }, []);
 
-  return playing;
+  function refreshSongData() {
+    getNowPlayingData();
+  }
+
+  useEffect(() => {
+    getNowPlayingData();
+  }, [getNowPlayingData]);
+
+  return { playing, refreshSongData };
 }
