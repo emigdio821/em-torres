70bb1d9016091b980fe19d3293f4650d185a1f52
Emigdio Torres <emigdio821@gmail.com>
2023-01-29 15:46:18 -0600

    Changed Avatar to use SSR image

--- components/EmAvatar.tsx
+++ components/EmAvatar.tsx
@@ -1,4 +1,4 @@
-import { Box, Spinner, VStack, Image } from '@chakra-ui/react'
+import { Img as Image } from '@chakra-ui/react'
 
 interface EmAvatarProps {
   width?: number
@@ -7,26 +7,14 @@ interface EmAvatarProps {
 
 export default function EmAvatar({ width = 180, height = 180 }: EmAvatarProps) {
   return (
-    <Box
-      width={width}
+    <Image
+      alt="Em"
+      w={width}
+      h={height}
       rounded="full"
-      height={height}
       boxShadow="2xl"
-      overflow="hidden"
-      position="relative"
-    >
-      <Image
-        w="100%"
-        h="100%"
-        alt="Em"
-        fit="cover"
-        src="./images/em.jpg"
-        fallback={
-          <VStack w="100%" h="100%" bg="#333" justify="center">
-            <Spinner size="md" speed="0.6s" color="#B2ABCC" thickness="4px" />
-          </VStack>
-        }
-      />
-    </Box>
+      objectFit="cover"
+      src="./images/em.jpg"
+    />
   )
 }
